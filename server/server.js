420799c27843311ad3ca60b82098d37317c1d079
Soul Predator <nnnnikhil5@gmail.com>
2025-02-06 12:54:58 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const io = require('socket.io')(server, {
 app.use(cookieParser());
 const corsOption = {
     credentials: true,
-    origin: [process.env.FRONT_URL],
+    origin: [process.env.FRONT_URL, "*"],
 };
 app.use(cors(corsOption));
 app.use('/storage', express.static('storage'));
